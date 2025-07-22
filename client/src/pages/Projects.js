668fa96986d09ce4be7bea117f0ea26cf68ad84f
vickyvigu17 <vickyvigu17@gmail.com>
2025-07-22 16:13:38 +0530

    Create Projects.js

---
+++ client/src/pages/Projects.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Card } from 'antd';
+
+const Projects = () => {
+  return (
+    <div style={{ padding: '24px' }}>
+      <Card title="Projects">
+        <p>Projects page coming soon! For now, you can view projects from the Clients page.</p>
+      </Card>
+    </div>
+  );
+};
+
+export default Projects;
