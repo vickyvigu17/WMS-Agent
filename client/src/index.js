940a3c99c0869cf0b8d258337e1fe52183f0a90c
vickyvigu17 <vickyvigu17@gmail.com>
2025-07-26 17:06:25 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ConfigProvider } from 'antd';
 import App from './App';
+import './App.css';
 import 'antd/dist/reset.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#667eea',
+          borderRadius: 8,
+          colorBgContainer: '#ffffff',
+        },
+      }}
+    >
+      <App />
+    </ConfigProvider>
   </React.StrictMode>
 );
